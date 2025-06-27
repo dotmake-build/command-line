faaefc6c63c4a320ec82d37c68a6e8d61e78259d
Dane Chambers <dchambers@costar.com>
2025-06-27 11:14:05 -0700

    Fix EnumUtil to Generate Multiple Flags if Necessary

--- src/DotMake.CommandLine.SourceGeneration/Util/EnumUtil.cs
+++ src/DotMake.CommandLine.SourceGeneration/Util/EnumUtil.cs
@@ -6,6 +6,7 @@ namespace DotMake.CommandLine.SourceGeneration.Util
 {
     public static class EnumUtil<TEnum> where TEnum : Enum
     {
+        private static readonly bool IsFlagsEnum = typeof(TEnum).IsDefined(typeof(FlagsAttribute), false);
         private static readonly Dictionary<TEnum, EnumInfo> Cache;
 
         static EnumUtil()
@@ -25,9 +26,23 @@ namespace DotMake.CommandLine.SourceGeneration.Util
 
         public static string ToFullName(TEnum value)
         {
-            return Cache.TryGetValue(value, out var enumInfo)
-                ? enumInfo.FullName
-                : string.Empty;
+            var fullName = string.Empty;
+
+            if (Cache.TryGetValue(value, out var enumInfo))
+            {
+                fullName = enumInfo.FullName;
+            }
+            else if (IsFlagsEnum)
+            {
+                var flags =
+                    Cache.Values
+                        .Where(x => !EqualityComparer<TEnum>.Default.Equals(x.Value, default) && value.HasFlag(x.Value))
+                        .Select(x => x.FullName);
+
+                fullName = string.Join(" | ", flags);
+            }
+
+            return fullName;
         }
 
         public static IEnumerable<EnumInfo> Enumerate()
